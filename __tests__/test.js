0efe06d271ec35156eea46340b2c4333e2062d3c
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-02 15:51:56 -0700

    good fail subtract murcury years from life expectancy

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,21 +5,24 @@ import{Planet} from './../src/age-calc';
 describe('Planet', () => {
   let murcury;
   beforeEach(() => {
-   murcury = new Planet("Murcury", .24, 25, 80)
+   murcury = new Planet("Murcury", .24, 82, 80)
   });
 
 test('should make a planet class with x planet name x planet year and y earth age', () => {
   expect(murcury.name).toEqual("Murcury");
   expect(murcury.planetYear).toEqual(0.24);
-  expect(murcury.personAge).toEqual(25);
+  expect(murcury.personAge).toEqual(82);
 });
 
 
 test('should multiply planet-Year and person-age to calculate planet-age ', () => {
-  expect(murcury.ageConversion()).toEqual(6);
+  expect(murcury.ageConversion()).toEqual(19.68);
 });
 
 test('should return the life expectancy in murcury years', () => {
-  expect(murcury.lifeConversion()).toEqual(19.2)
+  expect(murcury.lifeConversion()).toEqual(19.2);
+});
+test('should subtract murcury years from life expectancy to get years passed', () => {
+  expect(murcury.yearsP()).toEqual(.48);
 });
 });
