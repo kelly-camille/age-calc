ddf56fba4ca535dc57ee71185caa35dabb7c2e09
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-02 13:55:32 -0700

    WIP debug failing test by .000000000001

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,7 +5,7 @@ import{Planet} from './../src/age-calc';
 describe('Planet', () => {
   let murcury;
   beforeEach(() => {
-   murcury = new Planet("Murcury", .24, 30,)
+   murcury = new Planet("Murcury", .24, 30)
   });
 
 test('should make a planet class with x planet name x planet year and y earth age', () => {
@@ -13,15 +13,10 @@ test('should make a planet class with x planet name x planet year and y earth ag
   expect(murcury.planetYear).toEqual(.24);
   expect(murcury.personAge).toEqual(30);
 });
-});
 
 
-describe('Planet', () => {
-  let murcury;
-  beforeEach(() => {
-   murcury = new Planet("Murcury", .24, 30,)
-  });
 test('should multiply planet-Year and person-age to calculate planet-age ', () => {
   expect(murcury.calculatePlanetAge()).toEqual(7.2);
 });
+
 });
