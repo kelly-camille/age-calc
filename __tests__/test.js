48a4b1b2cedc0d74913421c5326d021e0cf49153
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-02 10:22:15 -0700

    wip first test

---
+++ __tests__/test.js
@@ -0,0 +1,11 @@
+// Create an function for each planet
+
+
+describe('Planet', () => {
+  let murcury;
+  beforeEach(() => {
+    murcury = new Planet
+test("should make a planet class with x year is y earth years", () => {
+});
+expect()
+});
