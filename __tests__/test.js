97d43eccd3e81a967bb64397053f9d3fcc3f1c3d
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-02 11:17:10 -0700

    test1 pass

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,9 +1,10 @@
 // Create an function for each planet
-
+import{Planet} from './../src/age-calc';
 
 describe('Planet', () => {
   let murcury;
   beforeEach(() => {
+  murcury = new Planet(.24)
   });
 test('should make a planet class with x year is y earth years', () => {
 expect(murcury.year).toEqual(.24)
---
+++ src/age-calc.js
@@ -0,0 +1,5 @@
+export class Planet {
+  constructor(year) {
+  this.year = year;
+  }
+};
