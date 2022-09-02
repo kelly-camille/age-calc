489fea9d018b52198a4206a10b8c43126614ef52
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-02 15:12:34 -0700

    WIP calculate life expectancy conversion

--- src/age-calc.js
+++ src/age-calc.js
@@ -1,12 +1,22 @@
 export class Planet {
-  constructor(name, planetYear, personAge) {
+  constructor(name, planetYear, personAge,) {
     this.name = name
     this.planetYear = planetYear;
     this.personAge = personAge;
+    // this.earthLifeExpectancy = earthLifeExpectancy;
   }
   ageConversion() {
-  this.planetAge = this.planetYear * this.personAge.toFixed(2);
+  this.planetAge = this.planetYear * this.personAge;
   return this.planetAge;
   }
 
+export class Earth
+
+  // lifeExpectancyConversion() {
+  //   this.planetExpectancy = this.earthLifeExpectancy * this.planetYear;
+  //   return this.planetExpectancy; 
+  // }
 }
+
+// if murcury.age > 19.2
+//   yearsPastExpected = murcury.age - lifeExpectancy
