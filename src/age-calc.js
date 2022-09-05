067e267747a743ba41f2b822aee9cb54ce2f03af
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-05 16:43:21 -0700

    clean code

--- src/age-calc.js
+++ src/age-calc.js
@@ -29,43 +29,4 @@ export class Planet {
   }
   }
 }
-  // yearsPassed() {
-  //   let yearsPassed = this.personAge - this.endOfLife;
-  //   let mercuryYearsPassed = yearsPassed * this.planetYear;
-  //   return mercuryYearsPassed;
-  // }
-
-
-
-  
-  // yearsLeft() {
-  //   let earthYearsLeft = this.endOfLife - this.personAge;
-  //   let mercuryYearsLeft = earthYearsLeft * this.planetYear;
-  //   return mercuryYearsLeft;
-  // }
-
-  // yearsPassed() {
-  //   let yearsPassed = this.personAge - this.endOfLife;
-  //   let mercuryYearsPassed = yearsPassed * this.planetYear;
-  //   return mercuryYearsPassed;
-  // }
-
-
-  // yearsP() {
-  //   this.yearsPassed = this.planetAge - this.lifeExpectM;
-  //   return this.yearsPassed
-  // }
-
-// attackRoll() {
-//   let damage = rollDice()
-//   let finalDamage = damage + this.attack;
-//   return finalDamage
-// }
-// defend(attackDamage) {
-//   let finalDamage = attackDamage - this.defense;
-//   return finalDamage
-// }
-// hpLoss(finalDamage) {
-//   this.hp -= finalDamage
-//   return this.hp;
-// 
+  g
